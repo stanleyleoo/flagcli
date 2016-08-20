41012d7cd947554f85bf4a430bd5cce48b813758
jansenkusuma <jansenkusuma@live.com>
2016-08-20 16:32:48 +0700

    adding name-B

--- src/FlagCli.java
+++ src/FlagCli.java
@@ -27,7 +27,7 @@ public class FlagCli {
         System.out.println(ANSI_BG_YELLOW_FL_BLUE + "==                              ==");
         System.out.println(ANSI_BG_YELLOW_FL_BLUE + "== WELCOME TO GIT COLLABORATION ==");
         System.out.println(ANSI_BG_YELLOW_FL_BLUE + "==         FLAG PROJECT         ==");
-        System.out.println(ANSI_BG_YELLOW_FL_BLUE + "==                              ==");
+        System.out.println(ANSI_BG_YELLOW_FL_BLUE + "==            JEKA              ==");
         System.out.println(ANSI_BG_YELLOW_FL_BLUE + "==================================");
         System.out.println("");
         System.out.println("Indonesia");
